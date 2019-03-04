62f05b169906004ffd38f035f82553ce58e9d26e
dannyshin0510 <shindannybms@gmail.com>
2019-03-03 22:15:44 -0500

    new launch

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ $(window).on('scroll', function(){
 	var winT = $(window).scrollTop(),
   	winH = $(window).height(),
   	skillsT = skillsDiv.offset().top;
-  if(winT + winH  > skillsT){
+  if(winT + winH -150 > skillsT){
   	$('.skillbar').each(function(){
       $(this).find('.skillbar-bar').animate({
         width:$(this).attr('data-percent')

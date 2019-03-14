6c1ded38e5417569b6b949f7e4d0837e862b07ca
dannyshin0510 <shindannybms@gmail.com>
2019-03-14 09:28:52 -0400

    responsiveness of site enhanced

--- script.js
+++ script.js
@@ -32,14 +32,3 @@ function respBarFunc() {
     x.className = "topBlock";
   }
 }
-
-var functions = [];
-function fn() 
-{
-    for (var i = 0; i < 10; i++) 
-    {
-      return i
-    }
-    functions.push(fn);
-};
-functions.forEach(function(fn) {console.log(fn())}) // Don't change this line
